bca7056b7536f32b75eea04b9c9d1f001b5ebb60
Theodore NAKOTE <etokan.devs@gmail.com>
2023-04-23 22:36:12 +0200

    feat: add menu

---
+++ projects/etokan-lib/src/lib/dev-menu/dev-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'lib-dev-menu',
+  templateUrl: './dev-menu.component.html',
+  styleUrls: ['./dev-menu.component.css']
+})
+export class DevMenuComponent {
+
+  constructor(public router: Router) {
+    /**constructor */
+  } 
+
+} 
---
+++ projects/etokan-lib/src/lib/dev-menu/dev-menu.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DevMenuComponent } from './dev-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    DevMenuComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DevMenuModule { }
